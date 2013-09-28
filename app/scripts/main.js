37747587cb7b198fc4356f6fca42f32a57c0321f
Joshua de Lange <joshua.delange@gmail.com>
2013-09-29 01:44:33 +0200

    adds first caching server integration

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,7 +2,41 @@ $(document).ready(function(){
 
 	'use strict';
 
+	var cachingEnabled = false,
+			keepCheckingForCaching = true,
+			cacheAddress = 'http://localhost:1337/',
+			checkForCachingServer = function(){
+
+		console.log('checing for caching server...') ;
+
+		$.ajax({
+			url: cacheAddress,
+			success: function(){
+				console.log('caching = on') ;
+				$('.cacheStatus').fadeIn() ;
+				cachingEnabled = true ;
+			},
+			error: function(){
+
+				console.log('caching not found') ;
+
+				if(keepCheckingForCaching) {
+					console.log('trying again') ;
+					setTimeout(function(){
+						checkForCachingServer() ;
+					}, 1000) ;
+				}
+
+			}
+		}) ;
+
+	} ;
+
+	checkForCachingServer() ;
+
 	$('.openDropbox').click(function(){
+
+		keepCheckingForCaching = false ;
 
 		var numOfFiles = 0,
 			fileNames = '' ;
@@ -17,11 +51,28 @@ $(document).ready(function(){
 					numOfFiles++;
 
 					var uniqueParts = this.link.replace('https://www.dropbox.com', ''),
-						pdfUrl = 'https://dl.dropboxusercontent.com' + uniqueParts + '?token_hash=AAE5nMnBD0EXGKzQIDq9hn5IcY8BApGhlxFqhml882aXKg&convert_doc_to_pdf=1&disable_range=1',
-						googleDocsViewer = 'http://docs.google.com/viewer?url=' + encodeURIComponent(pdfUrl) + '&embedded=true',
-						$iframe = $('<iframe />').attr('src', googleDocsViewer) ;
+						pdfUrl = 'https://dl.dropboxusercontent.com' + uniqueParts + '?token_hash=AAE5nMnBD0EXGKzQIDq9hn5IcY8BApGhlxFqhml882aXKg&dl=1',
+						googleDocsViewer = 'https://docs.google.com/viewer?url=' + encodeURIComponent(pdfUrl) + '&embedded=true' ;
 
-					$('.pdfs').append($iframe) ;
+					console.log('gdoc url', googleDocsViewer) ;
+
+					if(cachingEnabled){
+
+						// $('.pdfs').append($('<iframe />').attr('src', googleDocsViewer)) ;
+
+						var $docWrapper = $('<div />').addClass('documentWrapper'),
+								$page1 = $('<img />').attr('src', cacheAddress + encodeURIComponent(googleDocsViewer) + '/1') ;
+
+						$docWrapper.append($page1) ;
+
+						$('.pdfs').append($docWrapper) ;
+
+					}
+					else{
+
+						$('.pdfs').append($('<iframe />').attr('src', googleDocsViewer)) ;
+
+					}
 
 					if(numOfFiles === 1) {
 						fileNames = this.name ;
